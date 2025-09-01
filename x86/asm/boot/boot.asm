f8651418ff51eab18514dc632d188df0db1141bb
YaBoiDev12243 <logan.p.goodwin@icloud.com>
2025-09-01 16:00:31 -0400

    Update boot.asm

--- x86/asm/boot/boot.asm
+++ x86/asm/boot/boot.asm
@@ -1,28 +1,42 @@
-[org 0x7C00]      ; BIOS loads boot sector at 0x7C00
+[org 0x7C00]
 
 start:
-    cli             ; Disable interrupts while setting up
+    cli                     ; Disable interrupts while setting up
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0x7C00  ; Temporary stack
+    mov sp, 0x7C00          ; Temporary stack
 
-    ; Load the kernel (assume kernel.bin is at sector 2 on the disk)
-    mov ah, 0x02    ; BIOS read sector
-    mov al, 1       ; number of sectors to read at a time
-    mov ch, 0       ; cylinder
-    mov cl, 2       ; sector (kernel starts at sector 2)
-    mov dh, 0       ; head
-    mov dl, 0x80    ; drive (first HDD)
-    mov bx, 0x1000  ; ES:BX = 0x0000:0x1000, load kernel there
-    int 0x13        ; BIOS disk read
-    jc no_kernel    ; jump if error
+    ; -------------------------
+    ; Load kernel (multiple sectors)
+    ; -------------------------
+    mov bx, 0x1000          ; ES:BX = load address
+    mov cx, 2               ; starting sector (sector 2)
+    mov si, 8               ; number of sectors to load (adjust to kernel size)
+    mov dl, 0x80            ; first HDD
 
+load_sector:
+    mov ah, 0x02            ; BIOS read sector
+    mov al, 1                ; read 1 sector at a time
+    mov ch, 0
+    mov cl, cx               ; current sector
+    mov dh, 0
+    int 0x13
+    jc no_kernel             ; jump if error
+
+    add bx, 512              ; move pointer to next sector in memory
+    inc cx                    ; next sector
+    dec si
+    jnz load_sector
+
+    ; -------------------------
     ; Jump to kernel entry point
+    ; -------------------------
     jmp 0x1000:0000
 
 ; -------------------------
+; Error: kernel not found
 no_kernel:
     mov si, msg
 print_loop:
@@ -41,8 +55,6 @@ halt_loop:
 ; -------------------------
 msg db "Kernel not found!", 0
 
-; -------------------------
 ; Fill remaining bytes up to 510
-times 510 - ($-$$) db 0
-dw 0xAA55            ; Boot sector signature
-
+times 510-($-$$) db 0
+dw 0xAA55                ; Boot signature
