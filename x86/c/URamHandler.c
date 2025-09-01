66e294097221104c878f20b5859e69eae1f8214a
YaBoiDev12243 <logan.p.goodwin@icloud.com>
2025-09-01 15:51:14 -0400

    Create URamHandler.c

---
+++ x86/c/URamHandler.c
@@ -0,0 +1,23 @@
+#include "URamHandler.h"
+
+
+
+void URAM_Init() {
+    }
+
+unsigned int URAM_GetTotal() {
+    return 0; 
+}
+
+unsigned int URAM_GetFree() {
+    return 0; 
+}
+
+void* URAM_Alloc(unsigned int size) {
+  
+    return 0;
+}
+
+void URAM_Free(void* ptr) {
+    
+}
