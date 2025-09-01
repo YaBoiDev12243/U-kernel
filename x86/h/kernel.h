8d8f82d9082c89b8b82b5ddbb562cbd353743af9
YaBoiDev12243 <logan.p.goodwin@icloud.com>
2025-09-01 15:45:03 -0400

    Update kernel.h

--- x86/h/kernel.h
+++ x86/h/kernel.h
@@ -1,12 +1,12 @@
 #pragma once
 
-// Core subsystems
+
 #include "UCpuManager.h"
 #include "URamHandler.h"
 #include "UStoreHandler.h"
 #include "UGfxHandler.h"
 #include "UVgaHandler.h"
 
-// Optional: define kernel-wide constants
+
 #define U_VERSION "0.0.1"
 #define U_AUTHOR "YaBoiDev12243"
